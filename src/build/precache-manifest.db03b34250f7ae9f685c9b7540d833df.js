a8599c78e5271798fca1877d040b068c68716cde
aseighali <aseighali96@gmail.com>
2020-05-11 18:39:30 +1000

    first

---
+++ src/build/precache-manifest.db03b34250f7ae9f685c9b7540d833df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3847acd2f709daa8b832b348965417f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dff523a07853b79e8f8b",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "dc545c27ae1a54f1a242",
+    "url": "/static/js/2.5c4680b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5c4680b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dff523a07853b79e8f8b",
+    "url": "/static/js/main.bacc8926.chunk.js"
+  },
+  {
+    "revision": "e988c064c70788dbc50e",
+    "url": "/static/js/runtime-main.a56884a5.js"
+  }
+]);
